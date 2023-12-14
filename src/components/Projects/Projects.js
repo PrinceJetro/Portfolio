3f3651edd7981b1a7fa7a88dc83ac9fa9acc8a02
Adegbuyi Jephthah <114796977+PrinceJetro@users.noreply.github.com>
2023-12-14 20:16:41 +0100

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -23,68 +23,68 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath="https://res.cloudinary.com/dz209s6jk/image/upload/v1606413905/Challenges/ty4ppudcq94rtgj4rv6p.jpg"
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Todo App"
+              description="This ToDo app, crafted with React, empowers users to efficiently manage their tasks. Seamlessly add new tasks, mark them as completed, and effortlessly delete those no longer needed. With a clean and intuitive interface, this app simplifies task organization, helping you stay on top of your to-do list effortlessly.ch allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              ghLink="https://github.com/PrinceJetro/Todo-app"
+              demoLink="https://princejetro-todo-app.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath="https://res.cloudinary.com/dz209s6jk/image/upload/v1629017580/Screenshots/kklmlncc4yza3dvsjrtb.jpg"
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Calculator"
+              description="Experience the convenience of our React-based calculator app. Whether you're crunching numbers for work or school, this user-friendly tool provides a straightforward interface for basic arithmetic operations. Add, subtract, multiply, and divide with ease. "
+              ghLink="https://github.com/PrinceJetro/calculator-app"
+              demoLink="https://princejetro-calculator-app.vercel.app"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath="https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/kfaa5whc9ukiqp3tqseo.jpg"
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="LoopStudios Landing Page"
+              description="Dive into the world of web design with Loopstudios, a captivating challenge from Frontend Mentor. This project showcases cutting-edge frontend development skills, blending creativity and functionality seamlessly. "
+              ghLink="https://github.com/PrinceJetro/loopstudios"
+              demoLink="https://princejetros-loopstudios.vercel.app"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath="https://www.searchenginejournal.com/wp-content/uploads/2021/09/16-reasons-why-social-media-is-important-to-your-company-616d3200e6dc6-sej.png"
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Social Media Web App"
+              description="Welcome to a vibrant online community powered by Django, HTML, CSS, and JS! Our social media web application combines the robust backend capabilities of Django, leveraging a PostgreSQL database through Supabase for secure and efficient data storage"
+              ghLink="https://github.com/PrinceJetro/Jetro"
+              demoLink="https://jetrostagram.vercel.app"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath="https://www.google.com/url?sa=i&url=https%3A%2F%2Fdomatron.com%2Farticle%2Fmovie-app-name-ideas&psig=AOvVaw0FcRu2bPHEMI7edP8_rkQ0&ust=1702667534022000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPjt54vRj4MDFQAAAAAdAAAAABAD"
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Movie Trailer App"
+              description="Embark on a cinematic journey with our React-based Movie Trailer app, exclusively tailored for mobile users. Immerse yourself in the world of film trailers on-the-go. With a mobile-centric design, this app brings the magic of React to your fingertips. Explore and preview the latest movie trailers seamlessly, all within the convenience of your mobile device. Lights, camera, React - the silver screen experience, now in the palm of your hand!"
+              ghLink="https://github.com/PrinceJetro/sylva"
+              demoLink="https://sylva-x-jetro-movie-app.vercel.app/" 
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath="https://res.cloudinary.com/dz209s6jk/image/upload/v1633619397/Challenges/fhzpdnabrek50hvhftnl.jpg"
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="E-commerce Web Application"
+              description="Discover a world of online shopping with our React-powered eCommerce web app. Immerse yourself in a visually stunning and user-friendly interface designed for seamless navigation."
+              ghLink="https://github.com/PrinceJetro/Frontend-Mentor-E-commerce"
+              demoLink="https://princejetro-e-commerce.vercel.app/"
             />
           </Col>
         </Row>
